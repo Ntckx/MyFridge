50a27604e4bce61378a3cbbd2a958cf95d671ceb
Ntckx <Nontakorn.chatk@gmail.com>
2024-06-10 20:07:34 +0700

    backendNTCK

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import Routes from "./routes/route";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send('Hello Myfridge!!');
+app.get("/", (req: Request, res: Response) => {
+  return res.send("Hello Myfridge!!");
 });
 
-app.use('/', Routes);
-
+app.use("/", Routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
