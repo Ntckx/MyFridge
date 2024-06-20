ea8dc50cae41468864419957709ec3688c2e1fca
Ntckx <Nontakorn.chatk@gmail.com>
2024-06-20 22:29:41 +0700

    BackButton

--- Frontend/lib/widget/custom_appbar.dart
+++ Frontend/lib/widget/custom_appbar.dart
@@ -6,28 +6,39 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
 
-  const CustomAppBar({super.key, required this.title});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+  });
 
   Future<int> _getUserId() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt('userId') ?? 0;
   }
 
+  bool _isHomePage(GoRouter router) {
+    final location = router.routerDelegate.currentConfiguration?.uri.toString();
+    return location == '/home';
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: Navigator.of(context).canPop()
-          ? Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back_ios_new,
-                    color: AppColors.white),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            )
-          : null,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 10.0),
+        child: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new, color: AppColors.white),
+          onPressed: () {
+            final router = GoRouter.of(context);
+
+            if (router.canPop() && !_isHomePage(router)) {
+              router.pop();
+            } else {
+              router.go('/home');
+            }
+          },
+        ),
+      ),
       title: Text(
         title,
         style: Theme.of(context)
@@ -39,6 +50,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         IconButton(
           onPressed: () async {
             final userId = await _getUserId();
+            print('Navigating to notifications for user: $userId');
             GoRouter.of(context).go('/home/notifications', extra: userId);
           },
           icon: const Icon(Icons.notifications, color: AppColors.white),
