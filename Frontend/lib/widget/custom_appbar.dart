4057be9d5c226b04387d34622b7d57a096290d4a
virunpat theeranuluk <virunpat.thee@kmutt.ac.th>
2024-06-21 02:48:31 +0700

    [EDIT]app bar

--- Frontend/lib/widget/custom_appbar.dart
+++ Frontend/lib/widget/custom_appbar.dart
@@ -30,14 +30,64 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           icon: const Icon(Icons.arrow_back_ios_new, color: AppColors.white),
           onPressed: () {
             final router = GoRouter.of(context);
-
             if (router.canPop() && !_isHomePage(router)) {
-              router.pop();
+              router.pop('/');
             } else {
               router.go('/home');
             }
           },
         ),
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .headlineMedium!
+            .copyWith(color: AppColors.white),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () async {
+            final userId = await _getUserId();
+            print('Navigating to notifications for user: $userId');
+            GoRouter.of(context).go('/home/notifications', extra: userId);
+          },
+          icon: const Icon(Icons.notifications, color: AppColors.white),
+        ),
+        const SizedBox(width: 20),
+      ],
+      backgroundColor: AppColors.blue,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+
+class CustomAppBar2 extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar2({
+    super.key,
+    required this.title,
+  });
+
+  Future<int> _getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('userId') ?? 0;
+  }
+
+  bool _isHomePage(GoRouter router) {
+    final location = router.routerDelegate.currentConfiguration?.uri.toString();
+    return location == '/home';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: const  Padding(
+        padding: EdgeInsets.only(left: 10.0),
       ),
       title: Text(
         title,
