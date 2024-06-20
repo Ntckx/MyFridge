02197cd171b5d26be47b0577b9f797b08fc7a93b
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2024-06-20 18:25:09 +0700

    [EDIT] handle item maxInt

--- Frontend/lib/widget/dialog_box.dart
+++ Frontend/lib/widget/dialog_box.dart
@@ -28,8 +28,8 @@ class DialogBoxState extends State<DialogBox> {
       content: Form(
         key: _formKey,
         child: SingleChildScrollView(
-          child: Center(
-            child: Column(
+            child: Center(
+          child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -40,119 +40,138 @@ class DialogBoxState extends State<DialogBox> {
                       ),
                 ),
                 const SizedBox(height: 20),
-                TextFormField(
-                  style: const TextStyle(color: AppColors.darkblue),
-                  controller: widget.itemNameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Item name",
-                    hintText: 'e.g. Milk',
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text('Item Name',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: AppColors.darkblue,
+                          )),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.darkblue),
+                    controller: widget.itemNameController,
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.darkblue),
+                      ),
+                      hintText: 'e.g. Milk',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter an item name.';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter an item name.';
-                    }
-                    return null;
-                  },
-                ),
+                ]),
                 const SizedBox(height: 15),
-                FormField<int>(
-                  initialValue: int.tryParse(widget.quantityController.text),
-                  validator: (value) {
-                    if (value == null || value <= 0) {
-                      return 'Invalid quantity';
-                    }
-                    return null;
-                  },
-                  builder: (FormFieldState<int> state) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 120,
-                          height: 50,
-                          child: InputQty.int(
-                            qtyFormProps: const QtyFormProps(
-                                cursorColor: AppColors.darkblue,
-                                style: TextStyle(
-                                  color: AppColors.darkblue,
-                                )),
-                            decoration: QtyDecorationProps(
-                              qtyStyle: QtyStyle.btnOnRight,
-                              orientation: ButtonOrientation.vertical,
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: state.hasError
-                                      ? AppColors.red
-                                      : AppColors.darkblue,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Quantity',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              color: AppColors.darkblue,
+                            )),
+                    FormField<int>(
+                      initialValue:
+                          int.tryParse(widget.quantityController.text),
+                      validator: (value) {
+                        if (value == null || value <= 0) {
+                          return 'Invalid quantity';
+                        } else if (value > 99) {
+                          return 'Quantity cannot exceed 99 items';
+                        }
+                        return null;
+                      },
+                      builder: (FormFieldState<int> state) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 120,
+                              height: 50,
+                              child: InputQty.int(
+                                qtyFormProps: const QtyFormProps(
+                                    cursorColor: AppColors.darkblue,
+                                    style: TextStyle(
+                                      color: AppColors.darkblue,
+                                    )),
+                                decoration: QtyDecorationProps(
+                                  qtyStyle: QtyStyle.btnOnRight,
+                                  orientation: ButtonOrientation.vertical,
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: state.hasError
+                                          ? AppColors.red
+                                          : AppColors.darkblue,
+                                    ),
+                                  ),
+                                  btnColor: AppColors.darkblue,
+                                  fillColor: AppColors.grey,
+                                  borderShape: BorderShapeBtn.square,
+                                  plusBtn: const Icon(
+                                    Icons.arrow_drop_up_rounded,
+                                    color: AppColors.darkblue,
+                                  ),
+                                  minusBtn: const Icon(
+                                    Icons.arrow_drop_down_rounded,
+                                    color: AppColors.darkblue,
+                                  ),
+                                ),
+                                minVal: 1,
+                                initVal: int.tryParse(
+                                        widget.quantityController.text) ??
+                                    0,
+                                onQtyChanged: (val) {
+                                  state.didChange(val);
+                                  setState(() {
+                                    widget.quantityController.text =
+                                        val.toString();
+                                  });
+                                },
+                              ),
+                            ),
+                            if (state.hasError)
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                                child: Text(
+                                  state.errorText!,
+                                  style: const TextStyle(color: AppColors.red),
                                 ),
                               ),
-                              btnColor: AppColors.darkblue,
-                              fillColor: AppColors.grey,
-                              borderShape: BorderShapeBtn.square,
-                              plusBtn: const Icon(
-                                Icons.arrow_drop_up_rounded,
-                                color: AppColors.darkblue,
-                              ),
-                              minusBtn: const Icon(
-                                Icons.arrow_drop_down_rounded,
-                                color: AppColors.darkblue,
-                              ),
-                            ),
-                            minVal: 1,
-                            initVal:
-                                int.tryParse(widget.quantityController.text) ?? 0,
-                            onQtyChanged: (val) {
-                              state.didChange(val);
-                              setState(() {
-                                widget.quantityController.text = val.toString();
-                              });
-                            },
-                          ),
-                        ),
-                        if (state.hasError)
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 12),
-                            child: Text(
-                              state.errorText!,
-                              style:
-                                  const TextStyle(color: AppColors.red),
-                            ),
-                          ),
-                      ],
-                    );
-                  },
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    OutlinedButton(
-                      onPressed: widget.onCanceled,
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                            width: 1, color: AppColors.darkblue),
-                      ),
-                      child: const Text('Cancel'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          widget.onSaved();
-                        }
+                          ],
+                        );
                       },
-                      child: Text('Add',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(color: AppColors.white)),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(
+                          onPressed: widget.onCanceled,
+                          style: OutlinedButton.styleFrom(
+                            side: const BorderSide(
+                                width: 1, color: AppColors.darkblue),
+                          ),
+                          child: const Text('Cancel'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              widget.onSaved();
+                            }
+                          },
+                          child: Text('Add',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(color: AppColors.white)),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-          ),
-        ),
+              ]),
+        )),
       ),
     );
   }
